3f912c5185fe134e02a25184a6cce1a450e01a9b
Srinivasu Chattu <chsrinivas289@gmail.com>
2017-04-06 12:55:02 -0400

    Add project files.

---
+++ GA.RHS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GA.RHS.Startup))]
+namespace GA.RHS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
